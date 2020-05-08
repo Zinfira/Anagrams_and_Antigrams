3a1080ff0e2296adb4939c4daac1af18433d8be2
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-08 09:43:33 -0700

    Add first test to check if words are anagrams

---
+++ lib/anagrams_and_antigrams.rb
@@ -0,0 +1,6 @@
+class Anagram
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
+  end
+end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -1,2 +1,9 @@
 require('rspec')
 require('anagrams_and_antigrams')
+
+describe ('Anagram') do
+  it("checks if two words are anagrams and have same letters") do
+    anagram = Anagram.new('ruby', 'bury')
+    expect(anagram.anagram_checker).to(eq('These words are anagrams'))
+  end
+end
