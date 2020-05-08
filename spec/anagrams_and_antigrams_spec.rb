d2470e20973c76db91850cfa29d79c735389a7f5
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-08 10:20:56 -0700

    Add specs to check antigrams and fail test

--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -11,4 +11,9 @@ describe ('Anagram#anagram_checker') do
     anagram2 = Anagram.new('Tea', 'EaT')
     expect(anagram2.anagram_checker).to(eq('These words are anagrams'))
   end
+
+  it("checks if words are antigrams") do
+    antigram = Anagram.new('hi', 'bye')
+    expect(antigram.anagram_checker).to(eq("These words have no letter matches and are antigrams."))
+  end
 end
