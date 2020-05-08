823a2392ceb3ebcefb45a3cb4dba74eb309adf42
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-08 10:02:14 -0700

    Add anagram_checker method and pass test

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,6 +1,15 @@
 class Anagram
+  attr_accessor(:word1, :word2)
   def initialize(word1, word2)
     @word1 = word1
     @word2 = word2
+  end
+
+  def anagram_checker()
+    @word1 = word1.split('').sort
+    @word2 = word2.split('').sort
+    if @word1 == @word2
+      "These words are anagrams"
+    end
   end
 end
